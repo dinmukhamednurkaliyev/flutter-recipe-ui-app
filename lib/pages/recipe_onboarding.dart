478c1928788ce8fd59987baf7a813df3dd3c4a29
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-13 19:01:25 +0500

    build: migrate to very_good_analysis linter

--- lib/pages/recipe_onboarding.dart
+++ lib/pages/recipe_onboarding.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+/// A page that provides onboarding information for recipes.
 class RecipeOnboardingPage extends StatefulWidget {
+  /// Creates a new instance of [RecipeOnboardingPage].
   const RecipeOnboardingPage({super.key});
 
   @override
@@ -10,6 +12,6 @@ class RecipeOnboardingPage extends StatefulWidget {
 class _RecipeOnboardingPageState extends State<RecipeOnboardingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(backgroundColor: Colors.white, body: Column());
   }
 }
