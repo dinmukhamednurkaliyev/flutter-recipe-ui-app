ff01fe8e7ffe3a795949bf783d35963213184c16
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-13 18:55:06 +0500

    first commit

---
+++ lib/pages/recipe_onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecipeOnboardingPage extends StatefulWidget {
+  const RecipeOnboardingPage({super.key});
+
+  @override
+  State<RecipeOnboardingPage> createState() => _RecipeOnboardingPageState();
+}
+
+class _RecipeOnboardingPageState extends State<RecipeOnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
