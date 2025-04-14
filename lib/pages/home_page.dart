c8acf3677b924a6c8655db292067911da152acdc
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-14 16:42:28 +0500

    feat(ui): enhance home page with greeting and profile picture

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,24 +15,56 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: recipeAppBackground,
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Text.rich(
-                  TextSpan(
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  const Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Hello Dinmukhamed,\n',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        TextSpan(
+                          text: 'What do you want to eat today?',
+                          style: TextStyle(fontSize: 13, color: Colors.black45),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  Stack(
                     children: [
-                      TextSpan(
-                        text: "Hello Dima, \n",
-                        style: TextStyle(fontSize: 16),
+                      const CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.white,
+                        backgroundImage: NetworkImage(
+                          "https://avatars.githubusercontent.com/u/181092805?v=4",
+                        ),
+                      ),
+                      Positioned(
+                        right: 1,
+                        top: 1,
+                        child: Container(
+                          height: 9,
+                          width: 9,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.white),
+                            color: Colors.green,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
