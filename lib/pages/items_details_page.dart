29f3805710d2eb4425b87f8b0c0fad936602d379
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-04-15 12:18:47 +0500

    feat(ui): add back button, recipe owner info, and rating to item details page

--- lib/pages/items_details_page.dart
+++ lib/pages/items_details_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_recipe_ui_app/models/recipe.dart';
 
 /// A page that displays the details of items.
@@ -28,6 +29,114 @@ class _ItemsDetailsPageState extends State<ItemsDetailsPage> {
                 image: DecorationImage(
                   image: AssetImage(widget.recipeItems.image),
                   fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 20,
+              child: CircleAvatar(
+                backgroundColor: Colors.black38,
+                child: InkWell(
+                  onTap: () => Navigator.pop(context),
+                  child: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: size.height * 0.5,
+                width: size.width,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(60),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 50),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 22,
+                            backgroundImage: NetworkImage(
+                              widget.recipeItems.woner,
+                            ),
+                          ),
+                          const SizedBox(width: 10),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.recipeItems.wonerName,
+                                  maxLines: 1,
+                                  style: const TextStyle(
+                                    height: 0,
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                const SizedBox(height: 5),
+                                const Text(
+                                  '12 Recipes Shared',
+                                  style: TextStyle(
+                                    height: 0,
+                                    fontSize: 12,
+                                    color: Color.fromRGBO(0, 0, 0, 0.6),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    '${widget.recipeItems.rate}',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                  RatingBar.builder(
+                                    itemSize: 15,
+                                    initialRating: widget.recipeItems.rate,
+                                    unratedColor: Colors.grey.shade400,
+                                    itemBuilder:
+                                        (context, index) => const Icon(
+                                          Icons.star,
+                                          color: Colors.amberAccent,
+                                        ),
+                                    onRatingUpdate: (value) {},
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                '${widget.recipeItems.reviews} Reviews',
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: Color.fromRGBO(0, 0, 0, 0.6),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
